cd9a8353b4b5b918f18ab6d26766954ba4174559
Foo <foobarz076@gmail.com>
2023-12-17 20:03:45 +0800

    Initial

--- scripts-dev/lint.sh
+++ scripts-dev/lint.sh
@@ -11,7 +11,7 @@ if [ $# -ge 1 ]
 then
     files=$*
   else
-    files="my_project_name my-project-name tests"
+    files="foooothm_bot foooothm-bot tests"
 fi
 
 echo "Linting these locations: $files"
